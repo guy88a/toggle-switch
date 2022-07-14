9d339d5f205d96996e3528e35a9f697529bd84d1
Guy Aston <guy88a@gmail.com>
2022-07-15 01:24:54 +0300

    remove unused style

--- src/COMPONENTS/ToggleSwitch/index.jsx
+++ src/COMPONENTS/ToggleSwitch/index.jsx
@@ -8,19 +8,13 @@ export default function ToggleSwitch({ states = [], size = 16, name = 'test', te
         fontSize: `${size}px`,
     }
 
-    const labelInlineStyles = {
-        '&:after': {
-            width: `15px`,
-        }
-    }
-
     return(
         <div id="toggle-switch" className="wrapper">
             <div className="wrapper__content" style={wrapperInlineStyles}>
                 <div className="wrapper__content__text">{text}</div>
                 <div className="wrapper__content__input">
                     <input type="checkbox" checked={isChecked} onChange={onToggle} className="wrapper__content__input__checkbox" id={`toggle-switch-${name}`} name={name} />
-                    <label htmlFor={`toggle-switch-${name}`} className="wrapper__content__input__label" style={labelInlineStyles}></label>
+                    <label htmlFor={`toggle-switch-${name}`} className="wrapper__content__input__label"></label>
                 </div>
             </div>
         </div>
