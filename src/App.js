d597caba4ca616e56472f4919b73b11b25520a29
Guy Aston <guy88a@gmail.com>
2022-07-14 13:38:19 +0300

    project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './CSS/index.css';
+import ToggleSwitch from './CSS/COMPONENTS/ToggleSwitch';
 
 function App() {
+
+  const [isChecked, setIsChecked] = useState(false);
+
+  function onToggle() {
+    setIsChecked(!isChecked);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="ToggleSwitch" className="App">
+      <div className="toggle-switch-app-container">
+        <ToggleSwitch size={14} states={[isChecked, onToggle]} text={'אפשרות פילטור'} />
+      </div>
+      
     </div>
   );
 }
